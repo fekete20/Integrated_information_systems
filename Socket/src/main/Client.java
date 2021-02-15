25775668a40623fd09e2e4d12eb4ab9f161b6f2a
fekete20 <Antal@DESKTOP>
2021-02-15 22:57:21 +0100

    1st task of the course.

---
+++ Socket/src/main/Client.java
@@ -0,0 +1,143 @@
+package main;
+  import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+  
+  public class Client {
+  	Socket requestSocket;
+  	ObjectOutputStream out;
+  	ObjectInputStream in;
+  	String message;
+  
+  	Client() {
+  	}
+  	boolean cont;
+  	void run() {
+  		try {
+  			// 1. socket kapcsolat létrehozása
+  			requestSocket = new Socket("localhost", 8080);
+  			// 2. Input and Output streamek
+  			out = new ObjectOutputStream(requestSocket.getOutputStream());
+  			in = new ObjectInputStream(requestSocket.getInputStream());
+  			// 3: Kommunikáció
+  			do {
+  				try {
+  					//fájllista fogadása
+  					System.out.println((String) in.readObject());
+
+  					//írás vagy olvasás?  					
+  					System.out.println((String) in.readObject());
+  					Scanner scanner = new Scanner(System.in);
+  					String op = scanner.next();
+  					sendMessage(op); // billentyû elmegy a szervernek
+  					if (op.equals("u")) { // a kliens oldali állományt elküldjük a szervernek
+  						//fájlnév bekérése
+  						System.out.println((String) in.readObject());
+  						String filename = scanner.next(); 
+  						sendMessage(filename);
+  						// tartalom bekérése
+  						System.out.println((String) in.readObject());
+  	  					String content = scanner.next();
+	  					writeToLocalFile(filename, content);
+	  					sendMessage(upload(filename)); // fájl tartalmának elküldése a szervernek
+	  					System.out.println((String) in.readObject()); // sikeres volt-e a feltöltés
+  					}
+  					else if (op.equals("d")) { //létrehozzuk a fájlt a kliens oldalon és beírjuk a szerverrõl kapott adatokat
+
+  						// fájlnév bekérése
+  						System.out.println((String) in.readObject());
+  						String filename = scanner.next(); 
+  						sendMessage(filename);
+  						
+  						// kontent kiírása
+  						String content = (String) in.readObject();
+  						System.out.println(content);
+  	  					writeToLocalFile(filename, content);
+  					}
+  					else { System.err.println("Invalid operation.");
+  					}
+  					
+  					System.out.println((String) in.readObject());
+  					String c = scanner.next();
+  					if(c.equals("y")) {
+  						sendMessage("y");
+  						cont = true;
+  					} else {
+  						scanner.close();
+  						cont = false;
+  						sendMessage("n");
+  					}
+  				} catch (Exception e) {
+  					System.err.println("data received in unknown format");
+  				}
+  			} while (cont);
+  		} catch (UnknownHostException unknownHost) {
+  			System.err.println("You are trying to connect to an unknown host!");
+  		} catch (IOException ioException) {
+  			ioException.printStackTrace();
+  		} finally {
+  			// 4: Kapcsolat zárása
+  			try {
+  				in.close();
+  				out.close();
+  				requestSocket.close();
+  				
+  			} catch (IOException ioException) {
+  				ioException.printStackTrace();
+  			}
+  		}
+  	}
+  
+  	void sendMessage(String msg) {
+  		try {
+  			out.writeObject(msg);
+  			out.flush();
+  			System.out.println("client>" + msg);
+  		} catch (IOException ioException) {
+  			ioException.printStackTrace();
+  		}
+  	}
+  
+  	public static void main(String args[]) {
+  		Client client = new Client();
+  		client.run();
+  	}
+  	
+  	String upload(String filename) {
+  		File file = new File("files/" + filename);
+  		Scanner scanner;
+  		String content ="";
+		try {
+			scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+	  			content += scanner.nextLine() + "\n";
+	  		}
+	  		scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return content;
+  	}
+  	
+  	void writeToLocalFile(String filename, String text) {
+  		File file = new File("files/" + filename);
+		try {
+			if(file.createNewFile()) {
+				FileWriter fileWriter = new FileWriter("files/" + filename);
+				fileWriter.write(text);
+				fileWriter.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  		
+  	}
+  	
+  }
---
+++ Socket/src/main/Server.java
@@ -0,0 +1,149 @@
+package main;
+ import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+  
+  public class Server {
+  	ServerSocket providerSocket;
+  	Socket connection = null;
+  	ObjectOutputStream out;
+  	ObjectInputStream in;
+  	String message;
+  
+  	Server() {
+  	}
+  	boolean cont;
+  	void run() {
+  		try {
+  			// 1. szerver socket létrehozása
+  			providerSocket = new ServerSocket(8080, 10);
+  			// 2. kapcsolódásra várakozás
+  			connection = providerSocket.accept();
+  			// 3. Input és Output streamek megadása
+  			out = new ObjectOutputStream(connection.getOutputStream());
+  			in = new ObjectInputStream(connection.getInputStream());
+  			// 4. socket kommunikáció
+  			do {
+  				try {
+  					// fájlok listázása
+  					sendMessage("Files in the store directory: \n" + readFiles());
+  					
+  				//klienstõl fogadjuk, hogy u vagy d mûvelet lesz
+  					sendMessage("Download or upload? (d/u)");
+  					message = (String) in.readObject();
+  					System.out.println("client>" + message);
+  					
+  					
+  					if(message.equals("u")) { // nyitunk egy új fájlt és a klienstõl kapott adatokat kiírjuk a fájlba
+  						sendMessage("Please enter the filename with extension: ");
+  						String filename = message = (String) in.readObject();
+  	  					System.out.println("client>" + message);
+  	  					sendMessage("Please enter the content of the file: ");
+  						String text = message = (String) in.readObject();
+  	  					System.out.println("client>" + message);
+  						upload(filename, text);
+  						sendMessage("Uploaded.");
+  						}
+  					
+  					else if(message.equals("d")) { //felolvassuk a fájl tartalmát és elküldjük a kliensnek
+  						sendMessage("Please enter the filename with extension: ");
+						String filename = message = (String) in.readObject();
+	  					System.out.println("client>" + message);
+	  					sendMessage(download(filename));
+  						}
+  					else { System.out.println("Invalid operation.");
+  					}
+  					
+  					sendMessage("Do you want to continue? (y/n)");
+  					message = (String) in.readObject();
+  					System.out.println("client>" + message);
+  					
+  					if (message.equals("y")) {
+  					    cont = true;
+  					    
+  					} else {
+  						cont = false;
+  					}
+  				} catch (ClassNotFoundException classnot) {
+  					System.err.println("Data received in unknown format");
+  				}
+  			} while (cont);
+  		} catch (IOException ioException) {
+  			ioException.printStackTrace();
+  		} finally {
+  			// 4: kapcsolat lezárása
+  			try {
+  				in.close();
+  				out.close();
+  				providerSocket.close();
+  			} catch (IOException ioException) {
+  				ioException.printStackTrace();
+  			}
+  		}
+  	}
+  
+  	void sendMessage(String msg) {
+  		try {
+  			out.writeObject(msg);
+  			out.flush();
+  			System.out.println("server>" + msg);
+  		} catch (IOException ioException) {
+  			ioException.printStackTrace();
+  		}
+  	}
+  
+  	public static void main(String args[]) {
+  		Server server = new Server();
+  		while (true) {
+  			server.run();
+  		}
+  	}
+  	
+  	String readFiles() {
+  		File directory = new File("store");
+  		String files ="";
+  		for(File fileList : directory.listFiles()) {
+  			files += fileList + "\n";
+  		}
+  		return files;
+  	}
+  	
+  	void upload(String filename, String text) {
+  		File file = new File("store/" + filename);
+			try {
+				if(file.createNewFile()) {
+					FileWriter fileWriter = new FileWriter("store/" + filename);
+					fileWriter.write(text);
+					fileWriter.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  		
+  	}
+  	
+  	 String download(String filename) {
+  		File file = new File("store/" + filename);
+  		Scanner scanner;
+  		String content ="";
+		try {
+			scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+	  			content += scanner.nextLine() + "\n";
+	  		}
+	  		scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return content;
+  		
+  		 
+  	 }
+  }
